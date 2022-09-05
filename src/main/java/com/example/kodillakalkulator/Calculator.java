0ef0be6d044200ebf9d02bbf87e5e3142a34cae8
Jacek Ostapczuk <jacekostapczuk86@gmail.com>
2022-09-05 20:10:32 +0200

    First commit

---
+++ src/main/java/com/example/kodillakalkulator/Calculator.java
@@ -0,0 +1,13 @@
+package com.example.kodillakalkulator;
+
+public class Calculator {
+
+    public int addition (int a, int b) {
+    return int resultOfAddition = a+b;
+    }
+
+    public int subtraction (int a, int b) {
+        return int resultOfSubtraction = a-b;
+    }
+
+}
